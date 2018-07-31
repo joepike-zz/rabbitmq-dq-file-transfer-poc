bbb7c18209fd0b5069724f86dc8678ca148e7ae5
joe <joe@joes-MacBook-Pro.local>
2018-07-31 16:23:10 +0100

    added receive_logs.py - generates queue to accept message from exchange

---
+++ receive_logs.py
@@ -0,0 +1,27 @@
+
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')
+
+result = channel.queue_declare()
+queue_name = result.method.queue
+
+# we will add messages through the exchange 'logs' to the randomly named queue previously generated
+channel.queue_bind(exchange='logs',
+                   queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(properties, ch, method, body):
+    print(" [x] %r" % body)
+
+channel.basic_consume(callback,
+                      routing_key='',
+                      no_ack=True
+                      )
